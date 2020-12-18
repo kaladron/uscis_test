8726da4375f025646291cda40e8bf4dd1d8ff737
Jeff Bailey <jbailey@raspberryginger.com>
2020-12-18 14:12:43 -0800

    Load json for question text

--- lib/questionscreen.dart
+++ lib/questionscreen.dart
@@ -3,23 +3,50 @@
 // Button to trigger listening response
 // Bottom bar with Home, Prev, Next
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class QuestionStorage {
+  Future<String> readFile() async {
+    try {
+      var contents = await rootBundle.loadString('2008.json');
+
+      return contents;
+    } catch (e) {
+      return e.toString();
+    }
+  }
+}
 
 class QuestionScreen extends StatefulWidget {
   static const routeName = '/question';
+  final QuestionStorage storage = QuestionStorage();
   @override
   _QuestionScreenState createState() => _QuestionScreenState();
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  String _questionText = "ooga";
+
+  void initState() {
+    super.initState();
+    widget.storage.readFile().then((String value) {
+      setState(() {
+        _questionText = value;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Question'),
+        title: Text("Question"),
       ),
       body: QuestionWidget(
-        "hi",
+        _questionText,
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
