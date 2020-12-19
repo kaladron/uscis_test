7e03751cdaca1d4a33b82701c45f7afc76e3727e
Jeff Bailey <jbailey@raspberryginger.com>
2020-12-18 16:56:38 -0800

    Parse the questions into Question objects

--- lib/question.dart
+++ lib/question.dart
@@ -1,13 +1,35 @@
+import 'dart:convert';
+
 import 'package:flutter/services.dart';
 
 class QuestionStorage {
   Future<String> readFile() async {
-    try {
-      var contents = await rootBundle.loadString('2008.json');
-
-      return contents;
-    } catch (e) {
-      return e.toString();
+//    try {
+    var contents = await rootBundle.loadString('2008.json');
+    var data = jsonDecode(contents);
+    List<Question> questions = [];
+    for (Map i in data) {
+      questions.add(Question.fromJson(i));
     }
+    return questions[0].question;
+//    } catch (e) {
+//      // TODO(jeffbailey): Better error handling.
+//      return e.toString();
+//    }
   }
+}
+
+List<Question> moduleQuestionFromJson(String str) =>
+    List<Question>.from(json.decode(str).map((x) => Question.fromJson(x)));
+
+class Question {
+  final int number;
+  final String question;
+  // answers
+  // final bool over65;
+  // final int must_answer;
+
+  Question.fromJson(Map<String, dynamic> json)
+      : number = json['number'],
+        question = json['question'];
 }
