4f8ab35cca036a2b167b1a5e805fb9047cd9c148
Jeff Bailey <jbailey@raspberryginger.com>
2020-12-28 11:09:46 -0800

    Initial stemming and stopword removal of answer checking

--- lib/questioncontext.dart
+++ lib/questioncontext.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:uscis_test/question.dart';
+import 'package:collection/collection.dart';
+import 'package:stemmer/stemmer.dart';
 
 class QuestionContext extends ChangeNotifier {
   final BuildContext _context;
+  final PorterStemmer _stemmer = PorterStemmer();
 
   QuestionContext(this._context);
 
@@ -27,8 +30,42 @@ class QuestionContext extends ChangeNotifier {
     notifyListeners();
   }
 
+  // The following needs to happen in checkAnswer:
+  // Lower Case
+  // Strip punctuation
+  // Iterate over all answers
+  // Remove stopwords
+  // Convert numbers to words
+  // Apply stemmer
   bool checkAnswer(String answer) {
-    if (question.answers[0] == answer) return true;
+    List<String> answerTokens = List();
+    List<String> keyTokens = List();
+
+    for (var token in answer.split(' ')) {
+      token = _prepToken(token);
+      if (token != null) {
+        answerTokens.add(token);
+      }
+    }
+
+    for (var token in question.answers[0].split(' ')) {
+      token = _prepToken(token);
+      if (token != null) {
+        keyTokens.add(token);
+      }
+    }
+
+    print("Answer: " + answerTokens.toString());
+    print("Key: " + keyTokens.toString());
+
+    if (ListEquality().equals(answerTokens, keyTokens)) return true;
     return false;
+  }
+
+  String _prepToken(String token) {
+    token = token.toLowerCase();
+    if (token == "the") return null;
+    token = _stemmer.stem(token);
+    return token;
   }
 }
