95a50e9c46f23318db4cbebb7712406b2209b761
Jeff Bailey <jbailey@raspberryginger.com>
2021-01-05 08:15:22 -0800

    Pull questionlistitem into its own class.

--- lib/questionlistscreen.dart
+++ lib/questionlistscreen.dart
@@ -15,20 +15,36 @@ class QuestionListScreen extends StatelessWidget {
       body: ListView.builder(
         itemCount: context.watch<QuestionStorage>().questions.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            leading: Icon(Icons.question_answer),
-            trailing: Icon(Icons.star_outline),
-            title: Text(
-                context.watch<QuestionStorage>().questions[index].question),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => QuestionScreen(index)));
-            },
-          );
+          return QuestionListItem(index);
         },
       ),
+    );
+  }
+}
+
+class QuestionListItem extends StatefulWidget {
+  final int _index;
+
+  const QuestionListItem(this._index);
+
+  @override
+  _QuestionListItemState createState() => _QuestionListItemState();
+}
+
+class _QuestionListItemState extends State<QuestionListItem> {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(Icons.question_answer),
+      trailing: Icon(Icons.star_outline),
+      title: Text(
+          context.watch<QuestionStorage>().questions[widget._index].question),
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => QuestionScreen(widget._index)));
+      },
     );
   }
 }
