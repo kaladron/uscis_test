9daee15d1c171ba20ca6132ccb80eae40db21ee9
Jeff Bailey <jbailey@raspberryginger.com>
2021-01-12 08:58:38 -0800

    More sketching of how the test will look

--- lib/testscreen.dart
+++ lib/testscreen.dart
@@ -7,6 +7,19 @@ import 'package:flutter/material.dart';
 // it's considered mastered and is popped off the list and a new question is
 // brought in.
 
+// Three new preferences to introduce:
+// List<String> of mastered
+// List<String> of two-right
+// List<String> of one-right
+
+// The display screen will just have three icons on the screen:
+// A button to repeat the question
+// A button to listen to the answer
+// A next button that will appear
+
+// When you pass (at 6 questions for 2008) or fail (10 questions for 2008)
+// (20 questions for both in 2020) there needs to be a test results screen.
+
 class TestScreen extends StatelessWidget {
   static const routeName = '/test';
 
