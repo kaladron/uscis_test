b809ba95751fb5e209ac5820ce518abb49cabf34
Jeff Bailey <jbailey@raspberryginger.com>
2021-01-24 10:05:58 -0800

    Remove comment
    It was for the wrong file. =)

--- lib/testscreen.dart
+++ lib/testscreen.dart
@@ -1,17 +1,5 @@
 import 'package:flutter/material.dart';
 
-// TODO(jeffbailey): Going to want a new questioncontext here that is
-// unidirectional and just gives me the next item.
-// Start with a set of 10 questions, Put them up randomly.  Have to master the
-// answer 3 times in a row (pressing "show answer" counts as a failure) before
-// it's considered mastered and is popped off the list and a new question is
-// brought in.
-
-// Three new preferences to introduce:
-// List<String> of mastered
-// List<String> of two-right
-// List<String> of one-right
-
 // The display screen will just have three icons on the screen:
 // A button to repeat the question
 // A button to listen to the answer
