8397e73e5a754802dbdeaaa38a2c5af038c66657
Jeff Bailey <jbailey@raspberryginger.com>
2021-03-13 09:56:21 -0800

    Flag is now null safe
    It's still disabled because of a version conflict in the flutter_svg dep

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -18,7 +18,6 @@
 // About
 
 import 'package:flutter/material.dart';
-// ignore: import_of_legacy_library_into_null_safe
 // import 'package:flag/flag.dart';
 import 'package:provider/provider.dart';
 import 'package:uscis_test/prefs.dart';
